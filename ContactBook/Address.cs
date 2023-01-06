d96bad65da08b87cb8988130a7f955b9acfc3e14
Jenzi <jennydinh94@gmail.com>
2023-01-06 16:24:55 +0100

    Create Address.cs

---
+++ ContactBook/Address.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ContactBook
+{
+	public class Address
+	{
+        // Field members
+        public string StreetName { get; private set; }
+        public string StreetNo { get; private set; }
+
+        // Constructor - validate field inputs
+        public Address(string streetName, string streetNo)
+        {
+            if (string.IsNullOrEmpty(streetName))
+            {
+                throw new ArgumentNullException("Street name cannot be null");
+            }
+
+            if (string.IsNullOrEmpty(streetNo))
+            {
+                throw new ArgumentNullException("Street number cannot be null");
+            }
+
+            StreetName = streetName;
+            StreetNo = streetNo;
+        }
+
+    }
+}
+
