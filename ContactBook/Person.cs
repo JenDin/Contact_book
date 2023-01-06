41cb706174a03228c9ffc10427f0f6ea3a6c4509
Jenzi <jennydinh94@gmail.com>
2023-01-06 16:28:21 +0100

    Create Person.cs

---
+++ ContactBook/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ContactBook
+{
+    public class Person
+    {
+        // Field members
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string PhoneNo { get; private set; }
+        public Address Address { get; }
+
+        // Validate that each field member is set
+        public Person(string firstName, string lastName, string phoneNo, Address address)
+        {
+            if (string.IsNullOrEmpty(firstName))
+            {
+                throw new ArgumentNullException("First name cannot be null");
+            }
+
+            if (string.IsNullOrEmpty(lastName))
+            {
+                throw new ArgumentNullException("Last name cannot be null");
+            }
+
+            if (string.IsNullOrEmpty(phoneNo))
+            {
+                throw new ArgumentNullException("Phone number cannot be null");
+            }
+
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNo = phoneNo;
+            Address = address;
+        }
+    }
+}
