669efda5f82f33829b543ebd16647d8f1481ac01
Jenzi <jennydinh94@gmail.com>
2023-01-06 17:01:48 +0100

    Create ContactDirectory.cs
    Install Newtonsoft.Json

---
+++ ContactBook/ContactDirectory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ContactBook
+{
+	public class ContactDirectory
+	{
+        // Field members
+        private string _jsonFile = Directory.GetCurrentDirectory().ToString() + @"contactDirectory.json";
+        private List<Person> _contacts = new List<Person>();
+
+		// Constructor - deserialize all JSON objects
+        public ContactDirectory()
+		{
+			if(File.Exists(_jsonFile))
+			{
+				string jsonStr = File.ReadAllText(_jsonFile);
+
+				if(!string.IsNullOrEmpty(jsonStr))
+				{
+					_contacts = JsonConvert.DeserializeObject<List<Person>>(jsonStr);
+				}
+			}
+		}
+
+        // Save and serialize JSON object
+        private void SerializeAndSave()
+		{
+			string jsonStr = JsonConvert.SerializeObject(_contacts);
+			File.WriteAllText(_jsonFile, jsonStr);
+		}
+	}
+}
+
